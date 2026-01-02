70837c02f17a764d76dcda5e4fc5a0cfef6cb926
SuhasChoppala <choppalasuhas.02@gmail.com>
2026-01-02 12:34:57 +0530

    todos-config added

---
+++ src/collections/Todos.ts
@@ -0,0 +1,43 @@
+import { CollectionConfig } from 'payload'
+
+export const Todos: CollectionConfig = {
+  slug: 'Todos',
+  access: {
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'completed',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'media',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'createdAt',
+      type: 'date',
+      defaultValue: new Date(),
+    },
+    {
+      name: 'updatedAt',
+      type: 'date',
+      defaultValue: new Date(),
+    },
+  ],
+}
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -69,6 +69,7 @@ export interface Config {
   collections: {
     users: User;
     media: Media;
+    Todos: Todo;
     'payload-kv': PayloadKv;
     'payload-locked-documents': PayloadLockedDocument;
     'payload-preferences': PayloadPreference;
@@ -78,6 +79,7 @@ export interface Config {
   collectionsSelect: {
     users: UsersSelect<false> | UsersSelect<true>;
     media: MediaSelect<false> | MediaSelect<true>;
+    Todos: TodosSelect<false> | TodosSelect<true>;
     'payload-kv': PayloadKvSelect<false> | PayloadKvSelect<true>;
     'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
     'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
@@ -161,6 +163,19 @@ export interface Media {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "Todos".
+ */
+export interface Todo {
+  id: string;
+  title: string;
+  description: string;
+  completed?: boolean | null;
+  media?: (string | null) | Media;
+  createdAt: string;
+  updatedAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-kv".
  */
 export interface PayloadKv {
@@ -190,6 +205,10 @@ export interface PayloadLockedDocument {
     | ({
         relationTo: 'media';
         value: string | Media;
+      } | null)
+    | ({
+        relationTo: 'Todos';
+        value: string | Todo;
       } | null);
   globalSlug?: string | null;
   user: {
@@ -272,6 +291,18 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "Todos_select".
+ */
+export interface TodosSelect<T extends boolean = true> {
+  title?: T;
+  description?: T;
+  completed?: T;
+  media?: T;
+  createdAt?: T;
+  updatedAt?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
